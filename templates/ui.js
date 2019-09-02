f9c4c86b5a0f8dba52faf64f24f2a9e3ae2ad038
= <leandrotk100@gmail.com>
2019-09-02 10:15:45 -0300

    Interpolate ui template with component to be tested

--- templates/ui.js
+++ templates/ui.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import RegistrationWrapper from "../index";
+import ${componentToBeTested} from "../${componentToBeTested}";
 
-describe("<RegistrationWrapper />", () => {
+describe("${componentToBeTested}", () => {
   let renderedComponent;
   const children = <div>I am a child!</div>;
   const defaultProps = {
@@ -14,7 +14,7 @@ describe("<RegistrationWrapper />", () => {
   };
 
   const render = props =>
-    shallow(<RegistrationWrapper {...defaultProps} {...props} />);
+    shallow(<${componentToBeTested} {...defaultProps} {...props} />);
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -32,52 +32,6 @@ describe("<RegistrationWrapper />", () => {
       it("has the main props", () => {
         expect(componentProps).toMatchObject({
           onClickIcon: defaultProps.onClickAppBarIcon
-        });
-      });
-
-      describe("when isAppBarVisible is true", () => {
-        beforeEach(() => {
-          renderedComponent = render({ isAppBarVisible: true });
-          componentProps = renderedComponent.find(AnotherComponent).props();
-        });
-
-        it("has the props position and hideWithAnimation with correct values", () => {
-          expect(componentProps).toMatchObject({
-            hideWithAnimation: false,
-            position: "fixed"
-          });
-        });
-      });
-
-      describe("when isAppBarVisible is false", () => {
-        beforeEach(() => {
-          renderedComponent = render({ isAppBarVisible: false });
-          componentProps = renderedComponent.find(AnotherComponent).props();
-        });
-
-        it("has the props position and hideWithAnimation with correct values", () => {
-          expect(componentProps).toMatchObject({
-            hideWithAnimation: true,
-            position: "relative"
-          });
-        });
-      });
-
-      describe("when route.editingMode is true", () => {
-        it("has the prop icon with correct value", () => {
-          renderedComponent = render({ route: { editingMode: true } });
-          componentProps = renderedComponent.find(AnotherComponent).props();
-          expect(componentProps).toMatchObject({
-            icon: ArrowBack
-          });
-        });
-      });
-
-      describe("when route.editingMode is false", () => {
-        it("has not the icon prop", () => {
-          renderedComponent = render({ route: { editingMode: false } });
-          componentProps = renderedComponent.find(AnotherComponent).props();
-          expect(componentProps.icon).toBeUndefined();
         });
       });
     });
