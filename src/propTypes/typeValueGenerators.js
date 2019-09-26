8de7f9363b6cbc9f99b9de32b7ee99d55309228b
TK <leandrotk100@gmail.com>
2019-09-25 23:20:55 -0300

    Implement some type generators

--- src/propTypes/typeValueGenerators.js
+++ src/propTypes/typeValueGenerators.js
@@ -1,6 +1,23 @@
-const generateString = () => 'abs';
-const generateBool = () => true;
-const generateNumber = () => 123;
+const strings = [
+  'Ar',
+  'Trólebus',
+  'Pose',
+  'Descendente',
+  'Caldeira',
+  'Baterias',
+  'Rico',
+  'Beisebol',
+  'Verme',
+  'Anotar'
+];
+
+const bools = [true, false];
+
+const randomId = (n) => Math.floor(Math.random() * n);
+
+const generateString = () => strings[randomId(10)];
+const generateBool = () => bools[randomId(2)];
+const generateNumber = () => randomId(1000);
 const generateIntlShape = () => 'tricky';
 const generateInstanceOf = () => 'tricky';
 const generateWidthPropType = () => '100';
